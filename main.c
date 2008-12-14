f6c078eeaf96611c29186bea05932670f951765b
Bob <bob@example.com>
2008-12-14 17:25:58 -0500

    Prompt user for last name as well.
    (Note: I am committing as a different user here to show something later.)
    
    Signed-off-by: Bob <bob@example.com>

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 
 int main(int argc, char **argv)
 {
-	char name[255];
+	char first[255], last[255];
 
-	printf("Enter your name: ");
-	fgets(name, 255, stdin);
-	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+	printf("Enter your first name: ");
+	fgets(first, 255, stdin);
+	first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
-	printf("Hello %s!\n", name);
-	return 0;
+	printf("Now enter your last name: ");
+	gets(last); /* buffer overflow? what's that? */
+
+	printf("Hello %s %s!\n", first, last);
+ 	return 0;
 }
 
