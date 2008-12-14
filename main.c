a79befaa182bf6c693d893e570ac6cb3edd4a3b6
Nathan Jones <nathanj@insightbb.com>
2008-12-14 17:28:04 -0500

    Use proper grammar.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 	fgets(name, 255, stdin);
 	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
-	printf("Hello %s!\n", name);
+	printf("Hello, %s!\n", name);
 	return 0;
 }
 
